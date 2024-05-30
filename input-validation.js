3a15db0e16499a9be6a3821df8bb4c2f1113d868
Boalskii <148456596+Boalskii@users.noreply.github.com>
2024-05-30 13:50:21 +0200

    Input validation for email & password now works with js and sends an appropriate alert when validation fails.

--- input-validation.js
+++ input-validation.js
@@ -1,24 +1,61 @@
-// Validate Email
-function validateEmail(email) {
-    const patern = /^[a-zA-Z]\.[a-zA-Z]{2,}\@[a-zA-Z]{2,}\.[a-zA-Z]{2,3}$/;
-    if (!patern.test(email)) throw new Error(`${email} email not valid`);
+
+function validateEmail() {
+    let pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    // if (!pattern.test(email)) throw new Error(`${email} email not valid`);
+
+    let input = document.getElementById('email').value;
+
+    if (!(pattern.test(input))) {
+        // alert('Geef een valide emailadres op');
+        document.getElementById('email').value = '';
+        return true;
+    }
+
+    // pattern =
+
+}
+
+function validateFields(event, activateAlert, validateEmail, validatePassword) {
+    event.preventDefault();
+    activateAlert(validateEmail, validatePassword)
+}
+
+function activateAlert(email, password) {
+    let alertString = 'Geef een valide';
+
+    if (!password() && email()) {
+        alertString += ' email op.';
+        alert(alertString);
+    } else if (password() && !email()) {
+        alertString += ' wachtwoord op.';
+        alert(alertString);
+    } else if (password() && email()) {
+        alertString += ' email en wachtwoord op.';
+        alert(alertString);
+    }
 }
 
 // Validate Password
-function validatePassword(password) {
-    const patern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\S{8,}$/;
+function validatePassword() {
+    const pattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\S{8,}$/;
 
-    if (!(patern.test(password))) throw new Error(`password not strong enough`);
+    let input = document.getElementById('newPassword').value;
+
+    if (!(pattern.test(input))) {
+        // alert('Geef een valide wachtwoord op');
+        document.getElementById('newPassword').value = '';
+        return true
+    }
 }
 
 function showPassword() {
-    let x = document.getElementById("currentPassword");
-    let y = document.getElementById("newPassword");
-    if (x.type === "password") {
-        x.type = "text";
-    } else if (y.type === "password") {
-        y.type = "text"
+    let x = document.getElementById('currentPassword');
+    let y = document.getElementById('newPassword');
+    if (x.type === 'password') {
+        x.type = 'text';
+    } else if (y.type === 'password') {
+        y.type = 'text'
     } else {
-        x.type = "password";
+        x.type = 'password';
     }
 }
