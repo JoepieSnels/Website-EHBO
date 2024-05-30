c6cdbafd8d1b758cb54b6b2392b4991701e64ccd
JoepieSnels <j.snels@student.avans.nl>
2024-05-30 13:27:21 +0200

    email and password validation work

--- src/input-validation.js
+++ src/input-validation.js
@@ -1,24 +1,40 @@
+const e = require("express");
+
 // Validate Email
 function validateEmail(email) {
-    const patern = /^[a-zA-Z]\.[a-zA-Z]{2,}\@[a-zA-Z]{2,}\.[a-zA-Z]{2,3}$/;
-    if (!patern.test(email)) throw new Error(`${email} email not valid`);
+	const patern = /^[a-zA-Z]\.[a-zA-Z]{2,}\@[a-zA-Z]{2,}\.[a-zA-Z]{2,3}$/;
+	if (!patern.test(email)) throw new Error(`${email} email not valid`);
 }
 
 // Validate Password
 function validatePassword(password) {
-    const patern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\S{8,}$/;
+	const patern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])\S{8,}$/;
 
-    if (!(patern.test(password))) throw new Error(`password not strong enough`);
+	if (!patern.test(password)) throw new Error(`password not strong enough`);
 }
 
 function showPassword() {
-    let x = document.getElementById("currentPassword");
-    let y = document.getElementById("newPassword");
-    if (x.type === "password") {
-        x.type = "text";
-    } else if (y.type === "password") {
-        y.type = "text"
-    } else {
-        x.type = "password";
-    }
+	let x = document.getElementById("password");
+	if (x.type === "password") {
+		x.type = "text";
+	} else {
+		x.type = "password";
+	}
+}
+// Check if Email and Password fields are not empty
+function validateForm() {
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value;
+
+	if (email.trim() === "") {
+		var error = document.getElementById("emailValidation");
+
+		error.style.display = "block";
+	}
+
+	if (password.trim() === "") {
+		var error = document.getElementById("passwordValidation");
+
+		error.style.display = "block";
+	}
 }
