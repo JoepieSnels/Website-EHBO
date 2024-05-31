61f6ed567f4b39c25f221cda7441b3d1195c1b5e
SamvNoort <s.vannoort2@student.avans.nl>
2024-05-31 10:06:07 +0200

    Better responsiveness + validation of landline

--- verification.js
+++ verification.js
@@ -16,6 +16,7 @@ function verifyInputs() {
         } else {
             issues += 1;
             alert('invalide factuur email');
+            return;
         }
     }
 
@@ -26,6 +27,7 @@ function verifyInputs() {
     } else {
         issues += 1;
         alert('invalide postcode');
+        return;
     }
 
     // Check Birthday
@@ -36,14 +38,26 @@ function verifyInputs() {
     } else {
         issue += 1;
         alert('Je geboortedag is vandaag of in de toekomst, wat?');
+        return;
     }
 
     // Check phonenumber
-    const phoneNumberPattern = /^(?:\+31[ -]?)?(06|6)[ -]?[1-9][0-9]{7}$/
+    const phoneNumberPattern = /^(?:\+31[ -]?)?(06|6)[ -]?[1-9][0-9]{7}$/;
 
     if(phoneNumberPattern.test(document.getElementById('phoneNumber').value)) {
         alert('geldig telefoon nummer')
+        return;
     } else {
         alert('ongeldig telefoon nummer')
+    }
+
+    const landlinePattern = /^(?:\+31[ -]?)?[0-9]{2}[ -][0-9]{3} [0-9]{2} [0-9]{2}$/;
+
+    if(document.getElementById('landLine').value) {
+        if(landlinePattern.test(document.getElementById('landLine').value)) {
+            alert('geldig huistelefoon nummer');
+        } else {
+            alert('ongeldig huistelefoon nummer');
+        }
     }
 }
