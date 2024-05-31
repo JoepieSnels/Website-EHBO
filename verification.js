9094ddde17fbe7c8510ce69966d9a9faab122e5c
SamvNoort <s.vannoort2@student.avans.nl>
2024-05-31 10:23:39 +0200

    Reworked response message if invalid input

--- verification.js
+++ verification.js
@@ -1,63 +1,78 @@
-function verifyInputs() {
+function verifyInputs(event) {
+    event.preventDefault();
     let issues = 0
+    let issueMessage = 'Sommige velden zijn ongeldig ingevuld: ';
     // Check Email
     const emailPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
     if(emailPattern.test(document.getElementById('email').value)) {
-        // Do nothing, email is correct
+        // Valid input
     } else {
         issues += 1;
-        alert('invalide email');
+        issueMessage += 'Email';
     }
 
     // Check invoiceEmail
     if(document.getElementById('invoiceEmail').value) {
         if(emailPattern.test(document.getElementById('invoiceEmail').value)) {
-            // alert('Factuur email is goed');
+            // Valid input
         } else {
             issues += 1;
-            alert('invalide factuur email');
-            return;
+            if(issues > 1) {
+                issueMessage += ', ';
+            }
+            issueMessage += 'Factuur Email';
         }
     }
 
     // Check postalcode
     const postalcodePattern = /[0-9{4,4}]+[a-zA-Z{2,2}]/;
     if(postalcodePattern.test(document.getElementById('postalCode').value)) {
-        // alert('GOED');
+        // Valid input
     } else {
         issues += 1;
-        alert('invalide postcode');
-        return;
+        if(issues > 1) {
+            issueMessage += ', ';
+        }
+        issueMessage += 'Postcode'
     }
 
     // Check Birthday
     const date = new Date();
     const today = date.toISOString().split('T')[0];
     if(document.getElementById('dateOfBirth').value < today) {
-        alert('Je geboortedag is voor vandaag');
+        // Valid input
     } else {
-        issue += 1;
-        alert('Je geboortedag is vandaag of in de toekomst, wat?');
-        return;
+        issues += 1;
+        if(issues > 1) {
+            issueMessage += ', ';
+        }
+        issueMessage += 'Geboorte datum moet voor vandaag zijn';
     }
 
     // Check phonenumber
     const phoneNumberPattern = /^(?:\+31[ -]?)?(06|6)[ -]?[1-9][0-9]{7}$/;
 
     if(phoneNumberPattern.test(document.getElementById('phoneNumber').value)) {
-        alert('geldig telefoon nummer')
-        return;
+
     } else {
-        alert('ongeldig telefoon nummer')
     }
 
     const landlinePattern = /^(?:\+31[ -]?)?[0-9]{2}[ -][0-9]{3} [0-9]{2} [0-9]{2}$/;
 
     if(document.getElementById('landLine').value) {
         if(landlinePattern.test(document.getElementById('landLine').value)) {
-            alert('geldig huistelefoon nummer');
+            // Valid input
         } else {
-            alert('ongeldig huistelefoon nummer');
+            if(issues > 1) {
+                issueMessage += ', ';
+            }
+            issueMessage += 'huistelefoon/vaste telefoon nummer'
         }
+    }
+
+    if(issues > 0) {
+        alert(issueMessage)
+    } else {
+        sendMail(event);
     }
 }
