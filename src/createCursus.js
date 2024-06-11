b91c00a9701e449038c37c62d684feb18733dc7c
RenzeWest <rg.westerink@student.avans.nl>
2024-06-11 14:43:11 +0200

    Added certificates

--- src/createCursus.js
+++ src/createCursus.js
@@ -36,6 +36,33 @@ async function loadCreateCursus(requiredPermission) {
 
 	document.getElementById('unBlockID').style.display = 'block'; // Even controleren welke dit moet zijn
 
+    // Load certificates
+
+    try {
+
+        const apiRoute = 'https://api-ehbo.onrender.com/api/getCertificates';
+	    const validateResult = await fetch(apiRoute, {
+			headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+		});
+
+	    const toJson = await validateResult.json();
+        const arrayCertificates = toJson.data;
+
+        let netteString = '';
+
+        arrayCertificates.forEach(element => {
+            netteString += `<option>${element.Title}</option>`
+        });
+
+        document.getElementById('cursus-certificate').innerHTML += netteString;
+        
+    } catch (error) {
+        
+    }
+
+    
 }
 
 function alertNoAcces() {
@@ -55,18 +82,43 @@ async function createCursus() {
     const locationElement = document.getElementById('cursus-location');
     const datetimeElement = document.getElementById('cursus-datetime');
     const certificateElement = document.getElementById('cursus-certificate');
-    const deelnemersElement = document.getElementById('cursus-deelnemers');
+    const participantsElement = document.getElementById('cursus-deelnemers');
+    const costElement = document.getElementById('cursus-cost');
 
-    const validationResult = validateFieldValues(titleElement.value.trim(), descriptionElement.value.trim(), locationElement.value.trim(), datetimeElement.value.trim(), certificateElement.value.trim(), deelnemersElement.value.trim());
+    const validationResult = validateFieldValues(titleElement.value.trim(), descriptionElement.value.trim(), locationElement.value.trim(), 
+                                    datetimeElement.value.trim(), certificateElement.value.trim(), participantsElement.value.trim(), costElement.value.trim());
 
     if (validationResult === 1) {
         // The fields are correct, make the API call
         console.log('De velden zijn correct ingevuld. Cursus wordt aangemaakt...');
 
-        // TODO: Roep de API aan en controleer het resultaat
+        // Roep de API aan en controleer het resultaat
         try {
+
+            const projectResult = await fetch("https://api-ehbo.onrender.com/api/addCourse", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json; charset=UTF-8",
+                'Authorization': `bearer ${window.sessionStorage.getItem('jwtToken')}`
+
+			},
+			body: JSON.stringify({
+                title: titleElement.value.trim(),
+                description: descriptionElement.value.trim(),
+                datetime: datetimeElement.value.trim(),
+                cost: costElement.value.trim(),
+                maxParticipants: participantsElement.value.trim(),
+                location: locationElement.value.trim(),
+                certificatieTitle: certificateElement.value.trim() 
+			}),
+		});
+
+		const projectData = await projectResult.json();
+
+		console.log(projectData);
             
         } catch (error) {
+            console.error('Error: \n' + error);
             alert('Er is iets fout gegaan, probeer het later opnieuw, onze excusses...');
             return;
         }
@@ -97,7 +149,7 @@ async function createCursus() {
  * @param {datetime} datetime 
  * @param {string} certificate 
  */
-function validateFieldValues(title, description, location, datetime, certificate, deelnemers) {
+function validateFieldValues(title, description, location, datetime, certificate, deelnemers, cost) {
     
     if (title === '') {
         return 'Er is geen titel opgegeven, geef alstublieft een titel op';
@@ -136,6 +188,13 @@ function validateFieldValues(title, description, location, datetime, certificate
     if (deelnemers === '') {
         return 'Er is geen maximaal aantal deelnemers opgegeven, geef alstublief een maximaal aantal deelnemers op';
     }
+
+    if (cost === '') {
+        return 'Er is geen of een foutief bedrag voor de kosten opgegeven, geef alstublief een bedrag voor de kosten op';
+    } else if (cost.startsWith('-')) {
+        return 'U kunt geen negatief bedrag opgeven!';
+        
+    } 
     
     
     // Alles is correct, dus return 1!
