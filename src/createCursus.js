f544739a0bcc76a4a133510968bce79cd8053238
RenzeWest <rg.westerink@student.avans.nl>
2024-06-11 13:58:19 +0200

    Added the onload en input validation

--- src/createCursus.js
+++ src/createCursus.js
@@ -1,5 +1,49 @@
 
+async function loadCreateCursus(requiredPermission) {
+    console.log('On page load');
 
+	const jwtToken = window.sessionStorage.getItem('jwtToken'); // Haalt de token op uit de session
+	const permissions = window.sessionStorage.getItem('permissions'); // Haalt de permissies op
+
+	// Kijkt of de token een waarde heeft, zo nee is het null en stuurt hij de gebruiker naar de login page
+	if (jwtToken === null) { 
+		alertNoAcces();
+		return;
+	}
+
+	// Kijk of in de string van permissies de benodigde permissie zit
+	if (permissions === null || !permissions.match(requiredPermission)) {
+		alertNoAcces();
+		return;
+
+ 	} 
+
+	// Maak verzoek naar de server om te kijken of de token geldig is
+	const apiRoute = 'https://api-ehbo.onrender.com/api/validatetoken';
+	const validateResult = await fetch(apiRoute, {
+			headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+				'Authorization': `bearer ${jwtToken}`
+            }
+		});
+
+	const toJson = await validateResult.json();
+
+	if (toJson.message === 'Not authorized') {
+		alertNoAcces();
+		return;
+	}
+
+	document.getElementById('unBlockID').style.display = 'block'; // Even controleren welke dit moet zijn
+
+}
+
+function alertNoAcces() {
+	console.log('Not the right site permissions');
+	alert('You have no access to this page, redirecting to login');
+	window.location.href = './login.html';
+
+}
 
 
 async function createCursus() {
@@ -20,6 +64,12 @@ async function createCursus() {
         console.log('De velden zijn correct ingevuld. Cursus wordt aangemaakt...');
 
         // TODO: Roep de API aan en controleer het resultaat
+        try {
+            
+        } catch (error) {
+            alert('Er is iets fout gegaan, probeer het later opnieuw, onze excusses...');
+            return;
+        }
 
         // Fields will reset when the API cal has been succesfull
         titleElement.value = '';
@@ -55,6 +105,8 @@ function validateFieldValues(title, description, location, datetime, certificate
     
     if (description === '') {
         return 'Er is geen beschrijving opgegeven, geef alstublieft een beschrijving op';
+    } else if (description.length > 320) {
+        return 'De beschrijving die u door heeft gegeven is te lang, probeer het opnieuw met een kortere beschrijving!';
     }
 
     if (location === '') {
@@ -63,6 +115,18 @@ function validateFieldValues(title, description, location, datetime, certificate
 
     if (datetime === '') {
         return 'Er is geen datum en tijd opgegeven, geef alstublief een datum en tijd op';
+    } else {
+        // Controleer of de datum tenmiste 3 dagen vanaf vandaag is
+        const dateTimeSplit = datetime.split('T');
+	    
+        const currentDate = new Date();
+	    const minDate = new Date();
+	    minDate.setDate(currentDate.getDate() + 3);
+	    if (!(new Date(dateTimeSplit[0]) > minDate)) {
+            return `De opgegeven datum is niet minstens 3 dagen vanaf vandaag, vul dit alstublief opnieuw in en probeer het dan opnieuw!`
+        } else { 
+            console.log('PANIEEEK')
+        }
     }
 
     if (certificate === '') {
@@ -74,6 +138,6 @@ function validateFieldValues(title, description, location, datetime, certificate
     }
     
     
-    // Alles is correct!
+    // Alles is correct, dus return 1!
     return 1;
 }
