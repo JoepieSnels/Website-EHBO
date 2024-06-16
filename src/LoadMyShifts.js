fdd9c37d175357c9640c29c6e04ad380b719354a
Josinee <ja.besseling@student.avans.nl>
2024-06-16 20:26:54 +0200

    Update LoadMyShifts.js

--- src/LoadMyShifts.js
+++ src/LoadMyShifts.js
@@ -9,18 +9,21 @@ const testShift = {
 	Address: 'Test Straat 55, Breda',
 	Description: 'Barbell papa dienst lekker gewichten heffen en trainen enzo en geen hartaanval krijgen, dat soort dingen ja dit komt zeker goed. Barbell papa heeft U nodig om veilig te blijven'
 }
-
-function loadShifts(event) {
-
-    getShiftsFromDB(event)
-		.then((shift) => {
-			for (let i = 0; i < shift.length; i++) {
-				createShiftCard(shift[i]);
+//gebruikt
+function loadShifts(requiredPermission) {
+	if(requiredPermission) {
+		getShiftsFromDB()
+		.then((shifts) => {
+			for (let i = 0; i < shifts.length; i++) {
+				createShiftCard(shifts[i]);
+				
 			}
 		})
 		.catch((error) => {
 			console.log("Error loading projects:", error);
 		});
+	}
+    
 }
 
 function createShiftCard(shift) {
@@ -28,8 +31,8 @@ function createShiftCard(shift) {
 	shift.StartDate = shift.StartDate.split("T")[0];
 	shift.EndDate = shift.EndDate.split("T")[0];
 
-	if(shift.EndDate) {
-        shift.EndDate = '- ' + shift.EndDate;
+	if(shift.EndDate === shift.StartDate) {
+        shift.StartDate = '- ' + shift.EndDate;
     } else {
         shift.EndDate = '';
     }
@@ -52,8 +55,8 @@ function createShiftCard(shift) {
 }
 
 // Get an array of shifts from the database from this user
-async function getShiftsFromDB(event) {
-    event.preventDefault();
+async function getShiftsFromDB() {
+
     const jwtToken = window.sessionStorage.getItem("jwtToken");
 	console.log("Loading shifts from DB");
 
@@ -72,21 +75,24 @@ async function getShiftsFromDB(event) {
 	}
 }
 
-async function getShiftById(event, id) {
-	event.preventDefault();
+async function getShiftById(id) {
+
 	const jwtToken = window.sessionStorage.getItem("jwtToken");
+
 	console.log('Loading shift with ID: ' + id);
 
 	try {
-		const response = await fetch(`https://api-ehbo.onrender.com/api/getShiftById?shiftId=${id}`, {
+		const response = await fetch(`https://api-ehbo.onrender.com/api/getShiftById?shiftId=1`, {
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json; charset-UTF-8",
 				Authorization: `Bearer ${jwtToken}`,
 			}
+			
 		});
 		const dataJson = await response.json();
-		return dataJson.data;
+
+		return dataJson.data[0];
 	} catch(error) {
 		console.log('Error fetching data: ' + error);
 	}
@@ -95,36 +101,39 @@ async function getShiftById(event, id) {
 function goShiftDetailPage(id) {
 	document.location.href = `./MyShiftDetail.html?id=${id}`;
 }
-
-function loadShiftDetailPage(event) {
-	var url = document.location.href,
+//hier
+function loadShiftDetailPage(requiredPermission) {
+	if(getPermission(requiredPermission)) {
+		var url = document.location.href,
 		params = url.split("?")[1].split("&"),
 		data = {},
 		tmp;
-	for (var i = 0, l = params.length; i < l; i++) {
-		tmp = params[i].split("=");
-		data[tmp[0]] = tmp[1];
-	}
-
-	getShiftById(event, params)
-		.then((shift) => {
-			createShiftCard(shift, true);
+		for (var i = 0, l = params.length; i < l; i++) {
+			tmp = params[i].split("=");
+			data[tmp[0]] = tmp[1];
 		}
-
-	)
+		getShiftById(params)
+			.then((shift) => {
+				createDetailShiftCard(shift);
+			}
+		)
+	}
 }
 
 function createDetailShiftCard(shift) {
-	if(shift.EndDate) {
-        shift.EndDate = '- ' + shift.EndDate;
+	shift.StartDate = shift.StartDate.split("T")[0];
+	shift.EndDate = shift.EndDate.split("T")[0];
+
+	if(shift.EndDate === shift.StartDate) {
+        shift.StartDate = '- ' + shift.EndDate;
     } else {
         shift.EndDate = '';
     }
 
-    const item = `<div class="card project-list-card" onclick="goShiftDetailPage(${shift.ShiftId})">
+    const item = `<div class="card project-list-card">
                     <div class="card-header row" id="projectTitle">
-                        <p class=col-lg-10><b>Project:</b> ${shift.Title} </p>
-						<button type="button" class="col-lg-2 sol-sm-4 btn btn-danger" onclick="checkForRemoval(event, ${shift})" >Uitschrijven</button>
+                        <p class=col-9><b>Project:</b> ${shift.Title} </p>
+						<button type="button" class="col-3 btn btn-danger d-none d-md-block" onclick="checkForRemoval(event, ${shift})" >Uitschrijven</button>
                     </div>
                     <div class="card-body row project-card-body">
                         <p class="card-text col-lg-4 col-sm-6" id="shiftDate"><b>Datum:</b> ${shift.StartDate} ${shift.EndDate}</p>
@@ -134,6 +143,7 @@ function createDetailShiftCard(shift) {
                         <p class="card-text col-lg-4 col-sm-6" id="shiftNeededCertificates"><b>Benodigde certificaten:</b> Geen</p>
 						<p class="card-text col-lg-12 col-sm-12" id="shiftDescription"><b>Beschrijving:</b> ${shift.Description}</p>
                     </div>
+					<button type="button" class="col-12 btn btn-danger d-block d-md-none" onclick="checkForRemoval(event, ${shift})" >Uitschrijven</button>
                 </div>`;
 
 	document.getElementById("myShift").innerHTML += item;
